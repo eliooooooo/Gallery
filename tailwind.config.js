a2496d7936e315b72bfb6e4ba7676a64c73170b1
eliooooooo <eliott.burkle@gmail.com>
2024-10-09 11:50:34 +0200

    Style: change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          DEFAULT: '#4A2040',
+          DEFAULT: '#312c30',
           'light': '#9F6BA0',
         },
         secondary: {
-          DEFAULT: '#EC9DED',
-          'light': '#F5CCE8',
+          DEFAULT: '#f6cdf7',
+          'light': '#f5e9f1',
         }
       }
     },
